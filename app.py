6bd58ca73e58373f812903b3e5c1ea7ef4280897
one-numan <numamk786@gmail.com>
2024-03-23 22:42:10 +0530

    Update Code | Adding Comments

--- app.py
+++ app.py
@@ -1,46 +1,85 @@
-import ipapi
 from flask import Flask, render_template, url_for, redirect, request
+import ipapi  # Library
 
 
 app = Flask(__name__)
 
 
-@app.route('/maps')
-def maps():
-    return render_template("maps.html")
+@app.route('/')
+def index():
+    """Home Page 
+
+    Returns:
+        Render HTML Page: Index.html 
+    """
+    return render_template("index.html")
 
 
 @app.route('/error')
 def error():
+    """Exceptional Page 
+    If Any Error Occurs This Page Will be Rendered
+
+    Returns:
+       Render HTML Page: error.html 
+    """
     return render_template("error.html")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+    """If Page Not Found : 404
+
+    Args:
+        e (404): Not Found
+
+    Returns:
+        Render HTML Page: error.html 
+    """
     return render_template("error.html")
-
-
-@app.route('/')
-def index():
-    return render_template("index.html")
 
 
 @app.route('/myip')
 def myip():
+    """Render Page If Your Want to see Our Own IP
+    1. Call Function ipapi.location()
+    2. This Function Return dict_ip [Key:Value]
+    3. Pass this dict_ip into Html Page
+
+    If Any Exception Occurs Render Error Page
+    Returns:
+        Render HTML Page: myip.html with Dict_ip
+    """
     try:
-        a = ipapi.location()
-        return render_template("myip.html", a=a)
+        # Call Function ipapi.location()
+        my_own_ip = ipapi.location()
+        # This Function Return dict_ip [Key:Value]
+        #  Pass this dict_ip into Html Page
+        return render_template("myip.html", dict_ip=my_own_ip)
     except:
+        # Exceptional Error
         render_template('error.html')
 
 
 @app.route('/inputip', methods=["POST", "GET"])
 def inputip():
+    """Taking Users input IP and Find Its IP
+    1. Taking User Input IP from Form
+    2. Call Function and passing IP as Parameter ipapi.location(<ip>)
+    3. This Function Return dict_ip [Key:Value]
+    4. Pass this dict_ip into Html Page
+    Returns:
+        _type_: _description_
+    """
     if request.method == 'POST':
         try:
-            ip = request.form['search']
-            a = ipapi.location(ip=ip)
-            return render_template('myip.html', a=a)
+            # Taking User Input IP from Form
+            user_input = request.form['search']
+            # Call Function and passing IP as Parameter ipapi.location(<ip>)
+            # This Function Return dict_ip [Key:Value]
+            input_ip = ipapi.location(ip=user_input)
+            # Pass this dict_ip into Html Page
+            return render_template('myip.html', dict_ip=input_ip)
         except Exception as e:
             print(e)
             return render_template('error.html')
